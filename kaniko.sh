6e96eb6054a105541539838dd74f6a84c0066646
itaymelamed <itaymelamed@ymail.com>
2021-11-27 01:17:34 +0200

    added docker registry

--- kaniko.sh
+++ kaniko.sh
@@ -3,18 +3,24 @@ set -e pipefail
 echo "test ${tag}"
 
 
-mkdir -p /kaniko/.docker
-echo "creating auth config file"
-cat <<EOF >/kaniko/.docker/config.json
-{
-	"auths": {
-		"https://index.docker.io/v1/": {
-			"auth": "${echo -n ${password}:${username} | base64}"
-		}
-	}
-}
-EOF
+# mkdir -p /kaniko/.docker
+# echo "creating auth config file"
+# cat <<EOF >/kaniko/.docker/config.json
+# {
+# 	"auths": {
+# 		"https://index.docker.io/v1/": {
+# 			"auth": "${echo -n ${password}:${username} | base64}"
+# 		}
+# 	}
+# }
+# EOF
 
-cat /kaniko/.docker/config.json
+# cat /kaniko/.docker/config.json
 
-/usr/bin/executor --force --dockerfile=${dockerfile} --context=${context} --destination=minutemedia/${image}:${tag} --cache=true --cache-repo=minutemedia/${image}-cache 
+/usr/bin/executor \
+ --force \
+ --dockerfile=${dockerfile} \
+ --context=${context} \
+ --destination=docker-registry.docker-registry:5000/${image}:${tag} \
+ --cache=true \
+ --cache-repo=docker-registry.docker-registry:5000/${image}-cache 
