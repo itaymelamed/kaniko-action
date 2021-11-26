c812463e788a70d49779344db6c90667c7df0b95
itaymelamed <itaymelamed@ymail.com>
2021-11-27 00:39:57 +0200

    fixed

--- kaniko.sh
+++ kaniko.sh
@@ -15,4 +15,4 @@ cat <<EOF >/kaniko/.docker/config.json
 }
 EOF
 
-/usr/bin/executor --force --dockerfile=${dockerfile} --context=git://itaymelamed:${github_token}@github.com/FTBpro/mmservices-auto.git#refs/${tag} --destination=minutemedia/${image}:${tag} --cache=true --cache-repo=minutemedia/${image}-cache 
+/usr/bin/executor --force --dockerfile=${dockerfile} --context=git://itaymelamed:${github_token}@github.com/FTBpro/mmservices-auto.git#${tag} --destination=minutemedia/${image}:${tag} --cache=true --cache-repo=minutemedia/${image}-cache 
