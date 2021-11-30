1f35cc420390d828bbaa3157f1924794cf556624
itaymelamed <itaymelamed@ymail.com>
2021-11-30 11:13:40 +0200

    kaniko

--- kaniko.sh
+++ kaniko.sh
@@ -1,20 +1,21 @@
 set -e pipefail
-rm -rf /kaniko || true
-mkdir -p /kaniko/.docker || true
-echo "creating auth config file"
-cat <<EOF >/kaniko/.docker/config.json
-{
-  "insecure-registries" : ["docker-registry.docker-registry:5000"]
-}
-EOF
+cat kaniko.yaml
+# rm -rf /kaniko || true
+# mkdir -p /kaniko/.docker || true
+# echo "creating auth config file"
+# cat <<EOF >/kaniko/.docker/config.json
+# {
+#   "insecure-registries" : ["docker-registry.docker-registry:5000"]
+# }
+# EOF
 
-/usr/bin/executor \
- --cleanup \
- --insecure \
- --force \
- --dockerfile=${dockerfile} \
- --context=git://${gh_token}@github.com/FTBpro/mmservices-auto.git \
- --destination=docker-registry.docker-registry:5000/${image}:${tag} \
- --cache=true \
- --git branch=${branch} \
- --cache-repo=docker-registry.docker-registry:5000/${image}-cache
+# /usr/bin/executor \
+#  --cleanup \
+#  --insecure \
+#  --force \
+#  --dockerfile=${dockerfile} \
+#  --context=git://${gh_token}@github.com/FTBpro/mmservices-auto.git \
+#  --destination=docker-registry.docker-registry:5000/${image}:${tag} \
+#  --cache=true \
+#  --git branch=${branch} \
+#  --cache-repo=docker-registry.docker-registry:5000/${image}-cache
